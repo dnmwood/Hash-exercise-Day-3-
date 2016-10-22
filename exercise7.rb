f435cf50345a6a815f2f6ee30afe42a43ff64b62
David Wood <davidnmwood@gmail.com>
2016-10-22 13:47:06 -0400

    add cohort4

--- exercise7.rb
+++ exercise7.rb
@@ -1,4 +1,4 @@
-students = {:cohort1 => 34, :cohort2 => 42, :cohort3 => 22}
+students = {:cohort1 => 34, :cohort2 => 42, :cohort3 => 22, :cohort4 => 43}
 
 def cohort_list(array)
   array.each do |key,value|
