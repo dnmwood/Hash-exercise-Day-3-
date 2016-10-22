e948fa8b195e3be11ed29cba69766682c01dd8bf
David Wood <davidnmwood@gmail.com>
2016-10-22 14:10:26 -0400

    5% increase

--- exercise7.rb
+++ exercise7.rb
@@ -2,8 +2,18 @@ students = {:cohort1 => 34, :cohort2 => 42, :cohort3 => 22, :cohort4 => 43}
 
 def cohort_list(array)
   array.each do |key,value|
-    puts "#{key}: #{value} students"
+    puts "#{key}: #{value * 1.05} students"
   end
 end
 
-puts students.keys
+# def more_students(hash, amount_increased)
+#     hash.each do |key,value|
+#         new_amount_of_students = value * (1+(amount_increased.to_f/100))
+#         hash[key]=new_amount_of_students.to_i
+#     end
+#     cohort_list(hash)
+# end                                  ####still learning this code
+
+
+puts cohort_list(students)
+# # puts students.keys
