0802ed51bf9414fc6a8cd95fb37b11d1c26ba93b
David Wood <davidnmwood@gmail.com>
2016-10-22 13:44:28 -0400

    create method

--- exercise7.rb
+++ exercise7.rb
@@ -1,1 +1,9 @@
 students = {:cohort1 => 34, :cohort2 => 42, :cohort3 => 22}
+
+def cohort_list(array)
+  array.each do |key,value|
+    puts "#{key}: #{value} students"
+  end
+end
+
+cohort_list(students)
